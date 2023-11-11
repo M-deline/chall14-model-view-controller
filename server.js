f4f284a648530292df69517b58c64e2e756a1303
Madeline K <madkovanda@gmail.com>
2023-11-10 19:01:34 -0700

    update main handlebars to have correct href

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers/');
 const helpers = require('./utils/helpers');
@@ -8,7 +8,7 @@ const helpers = require('./utils/helpers');
 
 
 const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 // app.use(cors());
@@ -18,18 +18,18 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 
-// const sess = {
-//     secret: process.env.DB_SECRET,
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 
-// app.use(session(sess));
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
@@ -38,7 +38,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static('public'));
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
