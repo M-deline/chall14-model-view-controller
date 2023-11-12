483d79dbbc4777411b87b51c67261960e9ad5c0c
Madeline K <madkovanda@gmail.com>
2023-11-11 18:36:37 -0700

    update selectors

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -8,8 +8,8 @@ async function signupFormHandler(event) {
         const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
-                username,
-                password
+                username: document.querySelector('#username').value,
+    password: document.querySelector('#password').value
             }),
             headers: {
                 'Content-Type': 'application/json'
